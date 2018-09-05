3c7df714e582351cf8d689a9787bac723f067221
shree47 <35099650+shree47@users.noreply.github.com>
2018-09-05 13:26:28 +0530

    commneted line 32

--- src/test/java/de/mb/selenium/GoogleStartPageTest.java
+++ src/test/java/de/mb/selenium/GoogleStartPageTest.java
@@ -29,7 +29,7 @@ public class GoogleStartPageTest {
 		DesiredCapabilities capability = DesiredCapabilities.chrome();
 		URL gridUrl = new URL(gridServerUrl);
 		driver = new RemoteWebDriver(gridUrl, capability);
-		driver.get("https://samplep2000474034trial.hanatrial.ondemand.com/explore-ui5/");
+		//driver.get("https://samplep2000474034trial.hanatrial.ondemand.com/explore-ui5/");
 	}
 
 	@After
