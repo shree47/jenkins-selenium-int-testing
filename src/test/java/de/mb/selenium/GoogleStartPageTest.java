1660dd8e3e2beba0856d6578b72c4a4980560bcb
shree47 <35099650+shree47@users.noreply.github.com>
2018-09-06 12:50:46 +0530

    added delay everwhere

--- src/test/java/de/mb/selenium/GoogleStartPageTest.java
+++ src/test/java/de/mb/selenium/GoogleStartPageTest.java
@@ -45,6 +45,9 @@ public class GoogleStartPageTest {
 		
 		String bodyText = driver.findElement(By.tagName("body")).getText();
 		assertTrue(bodyText != null);
+		 try {
+           		 wait(10000);
+        	} catch (Exception e) {}
 				
 	}
 
@@ -53,6 +56,9 @@ public class GoogleStartPageTest {
 
 		String testTextField = driver.findElement(By.id("testTextField")).getText();
 		String testLabel = driver.findElement(By.id("testLabel")).getText();
+		 try {
+           		 wait(10000);
+        	} catch (Exception e) {}
 		
 		assertTrue(testTextField!=testLabel);	
 	}
